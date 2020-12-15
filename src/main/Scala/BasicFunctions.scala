7c6334c75329460a65d184f4c19b72d644cc283e
ichater <chaterizaak@gmail.com>
2020-12-15 17:23:21 +1100

    more work on classes

---
+++ src/main/Scala/BasicFunctions.scala
@@ -0,0 +1,7 @@
+object BasicFunctions extends App {
+  def add(x: Int, y: Int): Int = x + y
+  println(add(2, 4))
+
+  def stringifyNumber(x: Int, y: Int):String = s"The numbers ${x} and ${y} are now strings"
+  println(stringifyNumber(3,5))
+}
---
+++ src/main/Scala/Classes.scala
@@ -0,0 +1,72 @@
+import level04.WhiteBelt
+
+package object level04 {
+
+  sealed trait BJJ
+
+  case object WhiteBelt extends BJJ
+
+  case object BlueBelt extends BJJ
+
+  case object PurpleBelt extends BJJ
+
+  case object BrownBelt extends BJJ
+
+  case object BlackBelt extends BJJ
+
+}
+
+
+object Classes extends App {
+  type JobId = Int
+
+  type HumanId = Int
+
+  case class Job(name: String, description: String)
+
+  case class Human(name: String, optJobId: Option[JobId])
+
+  val jobsDatabase: Map[JobId, Job] = Map(
+    1 -> Job("Teacher", "Expert in their field"),
+    2 -> Job("Engineer", "Build things for people")
+  )
+
+  val humansDatabase: Map[HumanId, Human] = Map(
+    1 -> Human("Sally", None),
+    2 -> Human("Jenny", Some(1)),
+    3 -> Human("Timmy", Some(1024)) // jobId doesn't exist in jobsDatabase
+  )
+
+  def findHumanById(humanId: HumanId): Option[Human] = humansDatabase.get(humanId)
+
+  def logNameById( jobId: JobId)  : Option[String] = {
+    val maybeJob = jobsDatabase.get(jobId)
+    maybeJob match {
+      case Some(value) => Some(value.name)
+      case None => None
+    }
+  }
+
+
+  println(logNameById(2))
+
+  def mkBJJBeltOrNull(str: String): level04.BJJ =
+    str match {
+      case "white" => level04.WhiteBelt
+      case "blue" => level04.BlueBelt
+      case "purple" => level04.PurpleBelt
+      case "brown" => level04.BrownBelt
+      case "black" => level04.BlackBelt
+      case _ => null
+    }
+
+
+
+  println(s"izaak is a ${mkBJJBeltOrNull("purple")}")
+
+  case class Student(var rollno: Int = 1, var name: String = "Dan", var marks: Int=85)
+
+
+  val s1 = Student(2, "Izaak", 50)
+  println(s1)
+}
